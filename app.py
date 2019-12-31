7786a2e72033a69218b8dab95f3020ad5642c83d
Gon Gis <gon.eu.1987@gmail.com>
2019-12-31 17:35:45 -0300

    adding more endpoints for the planet CRUD

--- app.py
+++ app.py
@@ -68,6 +68,66 @@ def get_planets():
     return jsonify(result.data)
 
 
+@app.route('planet_details/<int:planet_id>', methods=['GET'])
+def planet_details(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        result = PlanetSchema.dump(planet)
+        return jsonify(result.data)
+    else:
+        return jsonify(message="planet not found"), 404
+
+
+@app.route('/add_planet', methods=['POST'])
+@jwt_required
+def add_planet():
+    planet_name = request.form['planet_name']
+    test = Planet.query.filter_by(planet_name=planet_name).first()
+    if test:
+        return jsonify("There is already a planet by that name"), 409
+    else:
+        planet_type = request.form['planet_type']
+        home_star = request.form['home_star']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
+
+        new_planet = Planet(planet_name=planet_name, planet_type=planet_type, home_star=home_star, mass=mass,
+                            radius=radius, distance=distance)
+        db.session.add(new_planet)
+        db.session.commit()
+        return jsonify(message="you added a planet"), 201
+
+
+@app.route('/update_planet', methods=['PUT'])
+@jwt_required
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name =request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="you updated a plannet"), 202
+    else:
+        return jsonify(message="Tha planet does not exists"), 404
+
+
+@app.route('/remove_planet/<int:planet_id', methods=['DELETE'])
+def remove_planet(planet_id: int):
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        db.session.delete(planet)
+        db.session.commit()
+        return jsonify(message="You delete a planet"), 202
+    else:
+        return jsonify(message="Tha planet does not exists"), 404
+
+
 @app.route('/register', methods=['POST'])
 def register():
     email = request.form['email']
@@ -112,6 +172,7 @@ def retrieve_password(email: str):
         return jsonify(message="password sent to " + email)
     else:
         return jsonify(message="Tha email doesn't exists")
+
 
 # Database Models
 class User(db.Model):
